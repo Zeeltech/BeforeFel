69a9ebe4df14d9c126c6b5e5bd2e64ee271e3e74
Zeel <zeeldesai2163@gmail.com>
2023-03-29 11:12:16 +0530

    Uri added

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://purchase-and-repair.onrender.com/",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
